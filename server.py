4c209b19e23e1ac6d7763e99981c7b52811048e8
Marcelo Lima <marcelo.lima@openbossa.org>
2013-12-05 18:18:58 -0300

    Fixed minor bugs

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import socket
-import threading
-import time
-import sqlite3
-import struct
+from threading import Thread
+from time import sleep
+from sqlite3 import connect, Error
+from struct import pack, unpack
 from sys import exit
 
 LOCAL_ADDR6 = 'fe80::7271:bcff:fe19:13c2'
@@ -10,22 +10,31 @@ LOCAL_PORT = 40000
 IFACE = 'eth0'
 
 def db_init():
-	con_db = sqlite3.connect('presence.db')
-	cur = con_db.cursor()
-	cur.execute("CREATE TABLE IF NOT EXISTS pir_reads(id INTEGER PRIMARY" +
-		" KEY AUTOINCREMENT, id_sensor TEXT NOT NULL, time TEXT)")
-	con_db.commit()
-	con_db.close()
+	try:
+		con_db = connect('presence.db')
+		cur = con_db.cursor()
+		cur.execute("CREATE TABLE IF NOT EXISTS pir_reads(id INTEGER \
+			PRIMARY" + " KEY AUTOINCREMENT, id_sensor TEXT       \
+			NOT NULL, time TEXT)")
+		con_db.commit()
+		con_db.close()
+	except Error, e:
+		print "Database error: " + str(e)
+		exit(1)
+
+	finally:
+		if con_db: 
+ 			con_db.close()
 
 def db_insert(data):
 	try:
-		con_db = sqlite3.connect('presence.db')
+		con_db = connect('presence.db')
 		cur = con_db.cursor()
 		cur.execute("INSERT INTO pir_reads(id_sensor, time) VALUES(\"" + 
-			str(data) + "\", strftime('%d-%m-%Y %H:%M:%S', " +
+			str(data) + "\", strftime('%d-%m-%Y %H:%M:%S', "       +
 				"'now','localtime'))") 
 		con_db.commit()
-	except sqlite3.Error, e:
+	except Error, e:
 		con_db.rollback()
 		print "Database error: " + str(e)
 
@@ -35,7 +44,7 @@ def connection_handler(payload, cliaddr):
 	print "Server: Connected by " + str(cliaddr)
 	
 	try:
-		data = struct.unpack("!11s", payload)[0]
+		data = unpack("!11s", payload)[0]
 	except struct.error, e:
 		print "Unpacking error: " + str(e)
 		return None
@@ -55,18 +64,21 @@ def server_init(sockaddr):
 		print "Socket error: " + str(e)
 		exit(1)
 	
+	db_init()
+
 	print ("Server listening on addr: " + str(sockaddr[0]) + " port: " +
 		str(sockaddr[1]))
-	
+
 	while True:
 		try:
 			payload, cliaddr = sk.recvfrom(1024)
 		except socket.error, e:
 			print "Recvfrom error: " + str(e)
-			continue
+			sk.close()
+			exit(1)
 		
-		time.sleep(1)
-		t = threading.Thread(target = connection_handler,
+		sleep(1)
+		t = Thread(target = connection_handler,
 			args =(payload, cliaddr,))
 		t.start()
 
@@ -91,16 +103,14 @@ def client_test(sockaddr, data):
 
 if __name__ == '__main__':
 	if not socket.has_ipv6:
-		raise Exception("IPV6 Address not configured")
+		raise Exception("Socket error: IPV6 Address not configured")
 		exit(1)
 
-	db_init()
-
 	sockaddr = (LOCAL_ADDR6, LOCAL_PORT)
-	t = threading.Thread(target=server_init, args=(sockaddr,))
+	t = Thread(target=server_init, args=(sockaddr,))
 	t.start()
 	
-	time.sleep(2)
+	sleep(2)
 	
 	client_test(sockaddr, "AB:CD:EF:GH")
 	client_test(sockaddr, "E1:E2:E3:E4")
